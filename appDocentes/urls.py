3933a4b3d0724a011748234f271b7f487d8be53d
esmac4213 <pejj9206mail.com>
2026-02-27 22:08:55 -0600

    correo recuperacion contraseña y bloqueo de matricula en edicion

--- appDocentes/urls.py
+++ appDocentes/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('proyectos/eliminar/<str:pk>/', views.eliminar_proyecto, name="eliminar_proyecto"),
     #Recuperacion de contraseñas django
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html' ), name='password_reset'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', html_email_template_name='registration/password_reset_email.html', subject_template_name='registration/password_reset_subject.txt'),name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_edit.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
